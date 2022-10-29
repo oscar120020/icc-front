072493e37806cde6ba461c4a113998c747b60eb2
oscar120020 <oscarmiguel12002@gmail.com>
2022-10-29 12:18:15 -0400

    add navigation loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { defaultTheme } from '../themes/defaultTheme'
-import { UIProvider } from '../context'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { defaultTheme } from "../themes/defaultTheme";
+import { UIProvider } from "../context";
+import NextNProgress from 'nextjs-progressbar';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient} >
+    <QueryClientProvider client={queryClient}>
       <UIProvider>
-        <ThemeProvider theme={defaultTheme} >
-          <CssBaseline/>
+        <ThemeProvider theme={defaultTheme}>
+          <CssBaseline />
+          <NextNProgress/>
           <Component {...pageProps} />
         </ThemeProvider>
       </UIProvider>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
