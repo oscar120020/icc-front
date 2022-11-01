20fd046af8bf47b5105d1c3ffaacb296e84ffbba
Vladimir Mercado <vladimirmercado2001@gmail.com>
2022-11-01 15:33:57 -0400

    add link

--- components/landing/PastContest.tsx
+++ components/landing/PastContest.tsx
@@ -1,5 +1,5 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import { Grid, Typography } from "@mui/material";
+import React, { useRef } from "react";
 import { orderScores, getNewerRanking } from "../../helpers/ranking";
 import { RankingResponse } from "../../interfaces/rankingsResponse";
 import RankingCard from "../cards/RankingCard";
@@ -9,22 +9,46 @@ interface Props {
 }
 
 export const PastContest = ({ rankings }: Props) => {
+  const { current } = useRef(getNewerRanking(rankings));
   return (
-    <Grid
-      container
-      sx={{ width: "100", display: "flex", justifyContent: "center" }}
-    >
-      {orderScores(getNewerRanking(rankings).scores).map((ranking, index) => {
-        if (index < 3) {
-          return (
-            <RankingCard
-              key={ranking.competitor.userName}
-              ranking={ranking}
-              index={index + 1}
-            />
-          );
-        }
-      })}
-    </Grid>
+    <>
+      <Typography
+        textAlign="center"
+        fontWeight="bold"
+        variant="h2"
+        color="primary"
+        sx={{ mb: 5 }}
+      >
+        Podio del &nbsp;
+        <a
+          href={current.url}
+          target="blank"
+          style={{ textDecoration: "underline", color: "#0ba7ce" }}
+        >
+          <Typography 
+            textAlign="center"
+            fontWeight="bold"
+            variant="h2"
+            color="primary"
+            sx={{ mb: 5, display: 'inline' }}>challenge más reciente</Typography>
+        </a>
+      </Typography>
+      <Grid
+        container
+        sx={{ width: "100", display: "flex", justifyContent: "center" }}
+      >
+        {orderScores(getNewerRanking(rankings).scores).map((ranking, index) => {
+          if (index < 3) {
+            return (
+              <RankingCard
+                key={ranking.competitor.userName}
+                ranking={ranking}
+                index={index + 1}
+              />
+            );
+          }
+        })}
+      </Grid>
+    </>
   );
 };
