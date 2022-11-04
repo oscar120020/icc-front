507d6214e1ad52ae086e2dc996ebc87bdaba1953
oscar120020 <oscarmiguel12002@gmail.com>
2022-11-03 23:13:16 -0400

    fix date of event condition

--- helpers/dateHelpers.ts
+++ helpers/dateHelpers.ts
@@ -3,11 +3,11 @@ export const getDatePlusOneDay = (date: Date, plus: number) => {
 };
 
 export const IsDateHigherThanNow = (date: Date): boolean => {
-  return new Date(date) > new Date();
+  return new Date(`${date} 01:00:00`) > new Date();
 };
 
 export const IsDateBetweenEvent = (date: Date): boolean => {
-  const startDate = new Date(`${date} 12:00:00`)
+  const startDate = new Date(`${date} 15:00:00`)
   const endDate = new Date(`${date} 16:30:00`)
 
   if (
